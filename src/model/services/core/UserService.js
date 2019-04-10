4d02886846127ec805303fdfe643cfcc49384cf6
Andrii Olexandrovich <awispack@gmail.com>
2019-04-10 15:32:04 +0300

    Optimization: got rid of "hasPicture" prop on user REST payloas

--- src/model/services/core/UserService.js
+++ src/model/services/core/UserService.js
@@ -2,24 +2,18 @@ import {API_SERVER_URL} from "../../api/rest/client-util";
 import {performRequest} from "../../api/rest/secureApi";
 import {API_BASE_PATH as OPEN_API_PATH} from "../../api/rest/openApi";
 
-const ALT_USER_PICTURE = "public/pictures/default-profile-picture.png";
-
 export const UserService = {
     getCurrentUser: _ => performRequest({
         method: "GET",
         path: "/user/info"
     }),
     composeUserPictureUrl: (user, refresh = false) => {
-        if (!!user && user.hasPicture) {
-            let pictureUrl = `${OPEN_API_PATH}/user/${user.id}/picture`;
-            if (refresh) {
-                const cacheBreaker = new Date().getTime();
-                pictureUrl = pictureUrl.concat("?t=" + cacheBreaker);
-            }
-            return API_SERVER_URL.concat(pictureUrl);
-        } else {
-            return ALT_USER_PICTURE;
+        let pictureUrl = `${OPEN_API_PATH}/user/${user.id}/picture`;
+        if (refresh) {
+            const cacheBreaker = new Date().getTime();
+            pictureUrl = pictureUrl.concat("?t=" + cacheBreaker);
         }
+        return API_SERVER_URL.concat(pictureUrl);
     },
     findUsers: query => {
         let criteria = query, searchByLogin = false;
