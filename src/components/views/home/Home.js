7abcb63590ddfb91da944ccb3ed91359a261b63a
Andrii Olexandrovich <awispack@gmail.com>
2019-04-15 19:01:46 +0300

    Incorporating native slds library for react - Home->ProfileIntoPanel->AccountDeactivation

--- src/components/views/home/Home.js
+++ src/components/views/home/Home.js
@@ -24,7 +24,7 @@ class Home extends React.Component {
         const commonClass = "slds-col slds-size_1-of-1 slds-p-around_xxx-small height-screen";
         return (
             <AppContextProvider>
-                <div className="Home container slds-grid slds-p-around_small slds-wrap">
+                <div className="home-container slds-grid slds-p-around_small slds-wrap">
                     <div className={Utility.join(commonClass, "slds-medium-size_5-of-12", "slds-large-size_4-of-12")}>
                         <ProfilePanel/>
                     </div>
--- src/components/views/home/components/profile-container/components/profile-card/settings/components/AccountDeactivation.js
+++ src/components/views/home/components/profile-container/components/profile-card/settings/components/AccountDeactivation.js
@@ -1,7 +1,12 @@
 import React from "react";
 import Events from "../../../../../../../../../model/events/application-events";
+import MediaObject from "@salesforce/design-system-react/module/components/media-object";
+import Icon from "@salesforce/design-system-react/module/components/icon";
+import Spinner from "@salesforce/design-system-react/module/components/spinner";
+import Button from "@salesforce/design-system-react/module/components/button";
+import InputIcon from "@salesforce/design-system-react/module/components/icon/input-icon";
+import Input from "@salesforce/design-system-react/module/components/input";
 
-import {Button, Icon, Input, Spinner} from "react-lightning-design-system";
 import {Navigation} from "../../../../../../../../../model/services/utility/NavigationService";
 import {LoginService} from "../../../../../../../../../model/services/core/AuthenticationService";
 import {Settings} from "../../../../../../../../../model/services/core/UserService";
@@ -26,7 +31,7 @@ class AccountDeactivation extends React.Component {
     handleDeleteAccount = _ => {
         const {user} = this.props, {username} = this.state;
         if (user.username !== username) {
-            this.setState({error: "Username is not confirmed"});
+            this.setState({username: "", error: "Username is not confirmed"});
         } else {
             CustomEvents.fire({eventName: Events.SETTINGS.LOCK, detail: {locked: true}})
                 .then(_ => this.setState({loading: true, error: ""}))
@@ -41,29 +46,25 @@ class AccountDeactivation extends React.Component {
         return (
             <div className="slds-grid slds-wrap slds-gutters">
                 <div className="slds-col slds-size_1-of-1">
-                    <div className="slds-scoped-notification slds-media slds-media_center" role="status">
-                        <div className="slds-media__figure">
-                            <Icon category="utility" size="medium" icon="info"/>
-                        </div>
-                        <div className="slds-media__body">
-                            <p>If you're going to delete your account in xMessenger, keep in mind
-                                that your data will not be purged. It'll reside in system and only SA will take
-                                final decision whether it should be cleared or not.</p>
-                        </div>
-                    </div>
+                    <MediaObject className="slds-scoped-notification" verticalCenter
+                                 figure={<Icon category="utility" name="info" size="medium"/>}
+                                 body={<p>If you're going to delete your account in xMessenger, keep in mind
+                                     that your data will not be purged. It'll reside in system and only SA will take
+                                     final decision whether it should be cleared or not.</p>}/>
                 </div>
                 <div className="slds-col slds-size_1-of-1 slds-m-top_small">
                     <div className="slds-float--left">
                         {loading && <div className="slds-is-relative slds-p-around_medium">
-                            <Spinner type="brand" container={false}/>
+                            <Spinner variant="brand" size="small"/>
                         </div>}
-                        {!loading && <Input iconRight="fallback" required
-                                            placeholder="Confirm your login"
-                                            value={username} error={error}
-                                            onChange={event => this.setState({username: event.target.value})}/>}
+                        {!loading && <Input
+                            iconRight={<InputIcon name="fallback" category="utility"/>}
+                            placeholder="Confirm your login" required
+                            value={username} errorText={error}
+                            onChange={event => this.setState({username: event.target.value})}/>}
                     </div>
                     <div className="slds-float--right">
-                        <Button className={`${loading && "slds-hide"}`} type="destructive"
+                        <Button className={`${loading && "slds-hide"}`} variant="destructive"
                                 onClick={this.handleDeleteAccount}>Delete account</Button>
                     </div>
                 </div>
