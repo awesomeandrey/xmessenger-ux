0c906cd64cdd65b001d2e76dc1e619853ab03da4
Andrii Olexandrovich <awispack@gmail.com>
2019-04-15 15:59:33 +0300

    Incorporating native slds library for react - Home->ProfileIntoPanel->ProfileCard

--- src/components/views/home/components/profile-container/components/profile-card/ProfileCard.js
+++ src/components/views/home/components/profile-container/components/profile-card/ProfileCard.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
 import Events from "../../../../../../../model/events/application-events";
 import SettingsModal from "./settings/SettingsModal";
 import ScalableImage from "../../../../../../common/components/images/scalable/ScalableImage";
 import PropsLoader from "../../../../../../common/components/loader/PropsLoader";
+import Dropdown from "@salesforce/design-system-react/module/components/menu-dropdown";
+import Button from "@salesforce/design-system-react/module/components/button";
+import MediaObject from "@salesforce/design-system-react/module/components/media-object";
 
-import {Button, DropdownButton, DropdownMenuItem} from "react-lightning-design-system";
 import {LoginService} from "../../../../../../../model/services/core/AuthenticationService";
 import {Utility} from "../../../../../../../model/services/utility/UtilityService";
 import {Navigation} from "../../../../../../../model/services/utility/NavigationService";
@@ -12,48 +14,64 @@ import {CustomEvents} from "../../../../../../../model/services/utility/EventsSe
 import {SessionStorage} from "../../../../../../../model/services/utility/StorageService";
 import {UserService} from "../../../../../../../model/services/core/UserService";
 
-class ProfileCard extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    handleLogout = _ => {
-        LoginService.logout()
-            .then(SessionStorage.clear)
-            .then(_ => Navigation.toLogin({}));
+const _onOpenSettings = _ => CustomEvents.fire({eventName: Events.SETTINGS.OPEN}),
+    _onLogout = _ => LoginService.logout().then(SessionStorage.clear).then(_ => Navigation.toLogin({})),
+    _onSelectOption = (option) => {
+        switch (option.value) {
+            case 1:
+                _onOpenSettings();
+                break;
+            case 2:
+                _onLogout();
+                break;
+        }
     };
 
-    handleOpenSettings = _ => {
-        CustomEvents.fire({eventName: Events.SETTINGS.OPEN});
-    };
-
-    render() {
-        const {user} = this.props;
-        return (
-            <div className="slds-media slds-media_center slds-has-flexi-truncate">
-                <SettingsModal {...this.props}/>
-                <div className="slds-media__figure slds-avatar slds-avatar_large">
+const ProfileCard = props => {
+    const {user} = props;
+    return (
+        <MediaObject
+            figure={
+                <div className="slds-avatar slds-avatar_large">
                     <ScalableImage title={user.name} src={UserService.composeUserPictureUrl(user, true)}/>
                 </div>
-                <div className="slds-media__body">
+            }
+            body={
+                <div className="slds-clearfix">
+                    <SettingsModal {...props}/>
                     <p className="slds-float_left">
-                        <span className="title-caps">{user.name}</span><br/>
-                        <span>{Utility.decorateUsername(user.username)}</span>
+                        <span className="slds-text-title_caps">{user.name}</span><br/>
+                        <span className="slds-text-color_weak">{Utility.decorateUsername(user.username)}</span>
                     </p>
                     <div className="slds-float_right">
-                        <Button type="neutral" className="mobile-visible-only"
-                                onClick={this.handleLogout}>Logout</Button>
-                        <DropdownButton type="icon-more" icon="setup" className="mobile-hidden">
-                            <DropdownMenuItem iconRight="settings"
-                                              onClick={this.handleOpenSettings}>Settings</DropdownMenuItem>
-                            <DropdownMenuItem iconRight="power"
-                                              onClick={this.handleLogout}>Logout </DropdownMenuItem>
-                        </DropdownButton>
+                        <Button variant="neutral" className="mobile-visible-only" onClick={_onLogout}>Logout</Button>
+                        <Dropdown className="mobile-hidden"
+                                  iconCategory="utility"
+                                  iconName="settings"
+                                  iconVariant="border-filled"
+                                  onSelect={_onSelectOption}
+                                  options={[
+                                      {
+                                          label: "Settings",
+                                          value: 1,
+                                          rightIcon: {
+                                              category: "utility",
+                                              name: "settings",
+                                          }
+                                      },
+                                      {
+                                          label: "Logout",
+                                          value: 2,
+                                          rightIcon: {
+                                              category: "utility",
+                                              name: "power",
+                                          }
+                                      },
+                                  ]}/>
                     </div>
                 </div>
-            </div>
-        );
-    }
-}
+            }/>
+    );
+};
 
 export default PropsLoader("user")(ProfileCard);
