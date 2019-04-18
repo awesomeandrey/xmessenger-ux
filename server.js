9ad5c132b15e8e05599f7aa7918127b47c067327
Andrii Olexandrovich <awispack@gmail.com>
2019-04-18 23:50:58 +0300

    Bugfix: caching static content

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const app = express();
 
 const PORT = process.env.PORT || 80;
 
-app.use("/public", express.static("public"));
-app.use("/assets", express.static("node_modules/@salesforce-ux/design-system/assets"));
+const cacheControl = {maxAge: "30d"};
+app.use("/public", express.static("public", cacheControl));
+app.use("/assets", express.static("node_modules/@salesforce-ux/design-system/assets", cacheControl));
 
 app.get("*", function (req, res) {
     res.setHeader("X-Frame-Options", "DENY");
