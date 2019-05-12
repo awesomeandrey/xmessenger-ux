8593d9e6ff125817538611eeeb009cf1fb2a2179
Andrii Olexandrovich <awispack@gmail.com>
2019-05-12 10:32:57 +0300

    Implemented status indicators (beta).

--- src/model/api/streaming/services/TopicsManager.js
+++ src/model/api/streaming/services/TopicsManager.js
@@ -1,21 +1,33 @@
+import Topics from "../core/topics";
 import TopicsToEvents from "../core/topics-to-events";
 
 import {subscribe, sendMessage} from "../core/topics-manager";
 import {CustomEvents} from "../../../services/utility/EventsService";
 
+/**
+ * Intended for devices which don't support service workers.
+ *
+ * @param _ none.
+ */
 export const subscribeFromClient = _ => {
-    const registrations = TopicsToEvents.map(topicToEvent => {
-        return {
-            route: topicToEvent.topic,
-            callback: payload => {
-                let eventDetails = topicToEvent.getEventDetails(payload);
-                CustomEvents.fire(eventDetails); // immediately fire application events;
-            }
-        };
-    });
+    const registrations = TopicsToEvents.filter(topicToEvent => topicToEvent.topic !== Topics.USER.INDICATOR_CHANGE)
+        .map(topicToEvent => {
+            return {
+                route: topicToEvent.topic,
+                callback: payload => {
+                    let eventDetails = topicToEvent.getEventDetails(payload);
+                    CustomEvents.fire(eventDetails); // immediately fire application events;
+                }
+            };
+        });
     subscribe(registrations);
 };
 
+/**
+ * Intended for rich online experience mode.
+ *
+ * @param pushFunc - function responsible for sending push notifications.
+ */
 export const subscribeFromServer = pushFunc => {
     if (!!pushFunc && typeof pushFunc === "function") {
         const registrations = TopicsToEvents.map(topicToEvent => {
